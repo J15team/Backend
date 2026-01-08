d5d431257f6e35b59ba3c743268de7c391642131
Admin SABAKAN <ka392561@gmail.com>
2026-01-08 10:53:22 +0900

    昇順にSubjectIDが並ぶようにする

--- src/main/kotlin/com/j15/backend/infrastructure/repository/jpa/JpaSubjectRepository.kt
+++ src/main/kotlin/com/j15/backend/infrastructure/repository/jpa/JpaSubjectRepository.kt
@@ -4,4 +4,8 @@ import com.j15.backend.infrastructure.entity.SubjectJpaEntity
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.stereotype.Repository
 
-@Repository interface JpaSubjectRepository : JpaRepository<SubjectJpaEntity, Long>
+@Repository
+interface JpaSubjectRepository : JpaRepository<SubjectJpaEntity, Long> {
+    /** 題材IDの昇順で全題材を取得 */
+    fun findAllByOrderBySubjectIdAsc(): List<SubjectJpaEntity>
+}
