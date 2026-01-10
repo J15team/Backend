dd254faee5957440feb9c14b8d52d0072c39794f
Admin SABAKAN <ka392561@gmail.com>
2026-01-10 15:05:43 +0900

    feat: add TagName property test with Kotest

--- build.gradle.kts
+++ build.gradle.kts
@@ -36,6 +36,9 @@ dependencies {
     runtimeOnly("org.postgresql:postgresql")
 
     testImplementation("org.springframework.boot:spring-boot-starter-test")
+    testImplementation("io.kotest:kotest-runner-junit5:5.8.0")
+    testImplementation("io.kotest:kotest-assertions-core:5.8.0")
+    testImplementation("io.kotest:kotest-property:5.8.0")
     testRuntimeOnly("com.h2database:h2")
 }
 
