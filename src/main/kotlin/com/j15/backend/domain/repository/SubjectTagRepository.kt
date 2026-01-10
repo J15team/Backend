96f9f365393ba022a241a0d595f4d8c2f7095b80
Admin SABAKAN <ka392561@gmail.com>
2026-01-10 15:08:07 +0900

    feat: add SubjectTagRepository interface

---
+++ src/main/kotlin/com/j15/backend/domain/repository/SubjectTagRepository.kt
@@ -0,0 +1,16 @@
+package com.j15.backend.domain.repository
+
+import com.j15.backend.domain.model.subject.SubjectId
+import com.j15.backend.domain.model.tag.Tag
+import com.j15.backend.domain.model.tag.TagId
+
+/** 題材-タグ関連リポジトリ（ドメイン層のインターフェース） */
+interface SubjectTagRepository {
+    fun findTagsBySubjectId(subjectId: SubjectId): List<Tag>
+    fun findSubjectIdsByTagIds(tagIds: List<TagId>): List<SubjectId>
+    fun addTagToSubject(subjectId: SubjectId, tagId: TagId)
+    fun removeTagFromSubject(subjectId: SubjectId, tagId: TagId)
+    fun removeAllTagsFromSubject(subjectId: SubjectId)
+    fun removeTagFromAllSubjects(tagId: TagId)
+    fun existsAssociation(subjectId: SubjectId, tagId: TagId): Boolean
+}
